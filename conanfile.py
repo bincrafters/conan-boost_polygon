2732fa738ef981322fad994a4ad58b1da3f7b5af
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:17 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostPolygonConan(ConanFile):
     name = "Boost.Polygon"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/polygon"
+    url = "https://github.com/bincrafters/conan-boost-polygon"
+    source_url = "https://github.com/boostorg/polygon"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "polygon"
@@ -16,7 +16,7 @@ class BoostPolygonConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
