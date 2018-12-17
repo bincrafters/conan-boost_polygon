98f7d15d6247d2589491aba1ae8dffff6fee236d
Rene Rivera <grafikrobot@gmail.com>
2018-12-17 14:02:41 -0600

    Initial 1.69.0 packages. [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,11 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.68.0@bincrafters/testing")
+base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostPolygonConan(base.BoostBaseConan):
     name = "boost_polygon"
     url = "https://github.com/bincrafters/conan-boost_polygon"
     lib_short_names = ["polygon"]
     header_only_libs = ["polygon"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_mpl"
-    ]
+    b2_requires = ["boost_config"]
